48ba1079c9be05a6ecef6f3be21fa4050d69264d
Cyro MBLabs <cyroMBlabs@MacBook-Pro.local>
2023-11-22 15:43:44 -0300

    feat: integration training and

--- NeuralProcess/neuralNetwork.js
+++ NeuralProcess/neuralNetwork.js
@@ -1,29 +1,30 @@
-import { CalculateRoute } from "../Training/gps.js";
+import { CalculateRoute } from '../Training/gps.js';
 
-export function neuralNetwork() {
-  let storyData = [];
-  let bestDistance = Infinity;
-  let count = 0;
-  let trainingNumber = 1000;
+export function neuralNetwork(initialPoint, finalPoint) {
+	console.log(initialPoint, 'Here');
+	let storyData = [];
+	let bestDistance = Infinity;
+	let count = 0;
+	let trainingNumber = 1000;
 
-  while (count < trainingNumber) {
-    count++;
-    storyData.push(CalculateRoute("F", "F")[0]);
-    bestDistance = Math.min(
-      storyData[storyData.length - 1].distance,
-      bestDistance
-    );
-  }
+	while (count < trainingNumber) {
+		count++;
+		storyData.push(CalculateRoute(initialPoint, finalPoint)[0]);
+		bestDistance = Math.min(
+			storyData[storyData.length - 1].distance,
+			bestDistance
+		);
+	}
 
-  const betterGeneration = storyData.filter(
-    (data) => data.distance <= bestDistance
-  );
+	const betterGeneration = storyData.filter(
+		(data) => data.distance <= bestDistance
+	);
 
-  const bestMapMind = betterGeneration[0].mapMind;
+	const bestMapMind = betterGeneration[0].mapMind;
 
-  const bestGenes = betterGeneration[0].data;
+	const bestGenes = betterGeneration[0].data;
 
-  const bestTrainingPath = betterGeneration[0].path;
+	const bestTrainingPath = betterGeneration[0].path;
 
-  return [bestGenes, bestMapMind, bestTrainingPath];
+	return [bestGenes, bestMapMind, bestTrainingPath];
 }
--- driver.js
+++ driver.js
@@ -1,80 +1,80 @@
-import { neuralNetwork } from "./NeuralProcess/neuralNetwork.js";
-import { Perimeter } from "./DataDistance/allRoute.js";
+import { neuralNetwork } from './NeuralProcess/neuralNetwork.js';
+import { Perimeter } from './DataDistance/allRoute.js';
 
-let BestTrainingPath = neuralNetwork()[2];
 export function DriverRoute(initialPoint, finalPoint) {
-  let city = neuralNetwork()[1];
-  let gene = neuralNetwork()[0];
-  let path = [initialPoint];
-  let actualCity = path[path.length - 1];
-  let weightParam = 1;
-  let count = 0;
+	let BestTrainingPath = neuralNetwork(initialPoint, finalPoint)[2];
+	let city = neuralNetwork(initialPoint, finalPoint)[1];
+	let gene = neuralNetwork(initialPoint, finalPoint)[0];
+	let path = [initialPoint];
+	let actualCity = path[path.length - 1];
+	let weightParam = 1;
+	let count = 0;
 
-  function isNotAllCitiesInList(path) {
-    let boolList = [];
-    for (let i = 0; i < city.length; i++) {
-      boolList.push(path.includes(city[i].name));
-    }
-    return boolList.includes(false);
-  }
+	function isNotAllCitiesInList(path) {
+		let boolList = [];
+		for (let i = 0; i < city.length; i++) {
+			boolList.push(path.includes(city[i].name));
+		}
+		return boolList.includes(false);
+	}
 
-  while (isNotAllCitiesInList(path) || path[path.length - 1] != finalPoint) {
-    let nextCity = "";
-    let minDistweight = Infinity;
-    for (
-      let i = 0;
-      i < city.find((data) => data.name === actualCity).connections.length;
-      i++
-    ) {
-      minDistweight = Math.min(
-        city.find((data) => data.name === actualCity).connections[i].dist +
-          city.find((data) => data.name === actualCity).connections[i].weight,
-        minDistweight
-      );
-    }
+	while (isNotAllCitiesInList(path) || path[path.length - 1] != finalPoint) {
+		let nextCity = '';
+		let minDistweight = Infinity;
+		for (
+			let i = 0;
+			i < city.find((data) => data.name === actualCity).connections.length;
+			i++
+		) {
+			minDistweight = Math.min(
+				city.find((data) => data.name === actualCity).connections[i].dist +
+					city.find((data) => data.name === actualCity).connections[i].weight,
+				minDistweight
+			);
+		}
 
-    nextCity = city
-      .find((data) => data.name === actualCity)
-      .connections.find(
-        (data) => data.dist + data.weight <= minDistweight
-      ).name;
+		nextCity = city
+			.find((data) => data.name === actualCity)
+			.connections.find(
+				(data) => data.dist + data.weight <= minDistweight
+			).name;
 
-    path.push(nextCity);
-    let rate = count;
-    if (rate >= gene.length) {
-      rate = gene.length - 1;
-    }
+		path.push(nextCity);
+		let rate = count;
+		if (rate >= gene.length) {
+			rate = gene.length - 1;
+		}
 
-    let factor = (Math.random() + 9 * gene[rate]) / 10;
-    city
-      .find((data) => data.name === actualCity)
-      .connections.find((data) => data.name === nextCity).weight +=
-      weightParam * factor;
+		let factor = (Math.random() + 9 * gene[rate]) / 10;
+		city
+			.find((data) => data.name === actualCity)
+			.connections.find((data) => data.name === nextCity).weight +=
+			weightParam * factor;
 
-    actualCity = nextCity;
+		actualCity = nextCity;
 
-    const finalPointErr =
-      path[path.length - 1] != finalPoint && path.length > city.length * 2;
+		const finalPointErr =
+			path[path.length - 1] != finalPoint && path.length > city.length * 2;
 
-    if (finalPointErr) {
-      count++;
-      path = [initialPoint];
-      actualCity = initialPoint;
-    }
-  }
-  if (Perimeter(BestTrainingPath) < Perimeter(path)) {
-    path = BestTrainingPath;
-  }
-  console.log("Distância percorrida: " + Perimeter(path));
-  console.log("Tentativas: " + count);
-  return path;
+		if (finalPointErr) {
+			count++;
+			path = [initialPoint];
+			actualCity = initialPoint;
+		}
+	}
+	if (Perimeter(BestTrainingPath) < Perimeter(path)) {
+		path = BestTrainingPath;
+	}
+	console.log('Distância percorrida: ' + Perimeter(path));
+	console.log('Tentativas: ' + count);
+	return path;
 }
 
 try {
-  console.log(DriverRoute("F", "F"), "final result");
+	console.log(DriverRoute('A', 'F'), 'final result');
 } catch (e) {
-  console.log(e);
-  console.log("Melhor resultado de treino");
-  console.log("Distances Driver: " + Perimeter(BestTrainingPath));
-  console.log(BestTrainingPath);
+	console.log(e);
+	console.log('Melhor resultado de treino');
+	console.log('Distances Driver: ' + Perimeter(BestTrainingPath));
+	console.log(BestTrainingPath);
 }
